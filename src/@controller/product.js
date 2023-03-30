5f4bb3a3e5b8576827b37ba91bb8198203480b11
fafkhe <115066487+fafkhe@users.noreply.github.com>
2023-03-30 05:04:31 +0430

    [NEW][API] editProduct

--- src/@controller/product.js
+++ src/@controller/product.js
@@ -8,19 +8,49 @@ const { Product } = Models;
 export default {
   createProduct: async (req, res, next) => {
     const { name, price, description, quantity, imgUrl } = req.body;
-    if(!name || !price|| !description || !quantity ) throw new AppError("bad request: insufficient input")
+    if (!name || !price || !description || !quantity)
+      throw new AppError("bad request: insufficient input");
     const thisUser = await authorizeAdmin(req.user);
 
-    const newProduct =  await Product.create({
+    const newProduct = await Product.create({
       name,
       price,
       description,
       quantity,
-      imgUrl
+      imgUrl,
     });
     res.status(200).json({
       data: newProduct,
+    });
+  },
+  editProduct: async (req, res, next) => {
+    const { name, price, description, quantity, imgUrl } = req.body;
+
+    const [thisUser, thisProduct] = await Promise.all([
+      authorizeAdmin(req.user),
+      Product.findById(req.params._id),
+    ]);
+    if (!thisProduct) throw new AppError("bad request: no such product found");
+
+    const updateProduct = await Product.findByIdAndUpdate(
+      req.params._id,
+
+      {
+        name,
+        price,
+        description,
+        quantity,
+        imgUrl,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        updateProduct
+      }
     })
   },
-
 };
--- src/@router/product-router.js
+++ src/@router/product-router.js
@@ -5,5 +5,8 @@ import catchAsync from "../@lib/catchAsync";
 const router = Router();
 
 router.post("/create-product", catchAsync(productController.createProduct));
+router.post("/:_id", catchAsync(productController.editProduct));
+router.get("/all-product", catchAsync(productController.allProduct));
+router.get("/single-product", catchAsync(productController.singleProduct));
 
 export default router;
